4bd1877075ddb3dbffc4848a078549ae740f43b7
Fortunat Mutunda <fotimutunda@gmail.com>
2017-11-07 21:34:41 +0200

    get weather by geolocation... do the avg calculations and return the three to the user

--- Controllers/WeatherController.cs
+++ Controllers/WeatherController.cs
@@ -1,8 +1,13 @@
 ﻿using System;
+using System.Linq;
 using System.Net;
+using System.Runtime.InteropServices.ComTypes;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.Azure.KeyVault.Models;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace WeatherApi.Controllers
 {
@@ -11,9 +16,11 @@ namespace WeatherApi.Controllers
     
     {
         private readonly WebClient _webClient = new WebClient();
-        
+        private const string OpenWeatherMapUrl = "https://api.openweathermap.org/data/2.5/weather?APPID=2dbf2d5f4c4ca8b8ec9d0ac3e13faed2&units=imperial&";
+        private const string DarkSky = "https://api.darksky.net/forecast/b9a41cd9c98e4230e85bd9e96629757c/";
+
         [HttpGet]
-        public IActionResult GetAll([FromQuery] double lon, [FromQuery] double lat )
+        public IActionResult GetAll([FromQuery] QueryString  queryString )
         {
             /*
              * 1. Get location by lat and long from both acc and map
@@ -22,24 +29,68 @@ namespace WeatherApi.Controllers
              * 4. Return the data to the user in json acc mam and avg 
              */
             
-            dynamic res = JsonConvert.DeserializeObject(GetTemperatureByLocation(lat, lon).Result);
-            return new ObjectResult(res["main"]["temp"]);
+            dynamic res = JsonConvert.DeserializeObject(GetTemperatureByLocation( queryString.lat , queryString.lon ).Result);
+            return new ObjectResult(res);
             
         }
 
-        [HttpGet("{id}")]
-        public IActionResult GetById()
+        
+        private async Task<string> GetTemperatureByLocation(double lat, double lon)
         {
-            string[] strings = {"Balance", "Mutunda"};
-            return new ObjectResult(strings);
+            string[] urls = {OpenWeatherMapUrl, DarkSky};
+            dynamic darkCloud = "";
+            dynamic openMap = "";
+
+            foreach (var url in urls)
+            {
+
+                if (url.Equals(""))
+                {
+                     return null;
+                }
+                else if (url.Contains("openweathermap"))
+                {
+                    openMap = await _webClient.DownloadStringTaskAsync($"{OpenWeatherMapUrl}lat={lat}&lon={lon}");
+                }
+                else
+                {
+                    darkCloud =  await _webClient.DownloadStringTaskAsync($"{DarkSky}{lat},{lon}");
+                }
+
+            }
+
+            darkCloud = JObject.Parse(darkCloud);
+            openMap = JObject.Parse(openMap);
+            
+            var average = CompareTempsAndFindAvg(darkCloud, openMap);
+
+            return "{ temperatureOne:" + darkCloud.currently.temperature + ", temperatureTwo:" + openMap.main.temp + ", average:"+ average  + " }";
+
         }
 
-        private async Task<string> GetTemperatureByLocation(double lon, double lat)
+
+
+        private static double CompareTempsAndFindAvg(dynamic darkCloud, dynamic openMap)
         {
-            var openWeatherMap = await _webClient.DownloadStringTaskAsync($"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&APPID=2dbf2d5f4c4ca8b8ec9d0ac3e13faed2&units=imperial");   
-            return openWeatherMap;
+            
+            double x = darkCloud.currently.temperature;
+            double y = openMap.main.temp;
+            
+            if (Math.Abs(x) > 0 && Math.Abs(y) > 0)
+            {
+                return Math.Abs((y + x) / 2);
+            }
+
+            return 0;
         }
-        
-        
+
+
+    }
+
+    public class QueryString
+    {
+        [BindRequired]
+        public double lat { get; set; }
+        public double lon { get; set; }
     }
 }
