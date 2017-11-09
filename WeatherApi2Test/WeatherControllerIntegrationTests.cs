4e7b2d7c65a41e43c1583fa86725f7931b121916
Fortunat Mutunda <fotimutunda@gmail.com>
2017-11-09 06:18:02 +0200

    Integration test

--- WeatherApi2Test/WeatherControllerIntegrationTests.cs
+++ WeatherApi2Test/WeatherControllerIntegrationTests.cs
@@ -1,11 +1,105 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Linq;
 using Xunit;
+using FluentAssertions;
+using System.Net.Http.Headers;
+using System.Text;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using WeatherApi2;
+
 namespace WeatherApi2Test
 {
     public class WeatherControllerIntegrationTests
     {
+        private readonly TestServer _server;
+        private readonly HttpClient _client;
         public WeatherControllerIntegrationTests()
         {
+            _server = new TestServer(new WebHostBuilder().UseStartup<Startup>() );
+            _client = _server.CreateClient();
+            _client.DefaultRequestHeaders.Accept.Clear();
+            _client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
         }
+
+        [Fact]
+        public async Task Get_Friendly_Message_With_No_LonLat()
+        {
+            var response = await _client.GetAsync("/api/weather");
+            response.EnsureSuccessStatusCode();
+            var resposnseString = await response.Content.ReadAsStringAsync();
+            var weath = JObject.Parse(resposnseString);
+            weath.Should().ContainKey("message");
+
+        }
+
+        [Fact]
+        public async Task Return_Error_With_One_Query_Lon()
+        {
+            var response = await _client.GetAsync("/api/weather?lon=26.73321547");
+            var resposnseString = await response.Content.ReadAsStringAsync();
+            var weath = JObject.Parse(resposnseString);
+            weath.Should().ContainKey("error");   
+        }
+        
+        [Fact]
+        public async Task Return_Error_With_One_Query_Lat()
+        {
+            var response = await _client.GetAsync("/api/weather?lat=26.73321547");
+            var resposnseString = await response.Content.ReadAsStringAsync();
+            var weath = JObject.Parse(resposnseString);
+            weath.Should().ContainKey("error");   
+        }
+        
+        
+        [Fact]
+        public async Task Return_Other_message_with_nul_Lon_And_Lat()
+        {
+            var response = await _client.GetAsync("/api/weather?lat=&lon");
+            var resposnseString = await response.Content.ReadAsStringAsync();
+            var weath = JObject.Parse(resposnseString);
+            weath.Should().ContainKey("error");   
+        }
+        
+        [Fact]
+        public async Task Test2()
+        {
+            var response = await _client.GetAsync("/api/weather?lon=&lon");
+            var resposnseString = await response.Content.ReadAsStringAsync();
+            var weath = JObject.Parse(resposnseString);
+            weath.Should().ContainKey("error");   
+        }
+        
+        [Fact]
+        public async Task Test3()
+        {
+            var response = await _client.GetAsync("/api/weather?lat=&lat");
+            var resposnseString = await response.Content.ReadAsStringAsync();
+            var weath = JObject.Parse(resposnseString);
+            weath.Should().ContainKey("error");
+        }
+        
+        [Fact]
+        public async Task Succeful_Call()
+        {
+            var response = await _client.GetAsync("/api/weather?lat=42.023949&lon=-93.647595");
+            var resposnseString = await response.Content.ReadAsStringAsync();
+            var weath = JObject.Parse(resposnseString);
+            weath.Should().ContainKey("name").And.ContainKey("average").And.ContainKey("temperatureTwo").And.ContainKey("temperatureOne");
+        }
+        
+        
+        
+        
+        
+        
+
+
     }
 }
--- WeatherApi2Test/WeatherControllerUnitTests.cs
+++ WeatherApi2Test/WeatherControllerUnitTests.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using WeatherApi2.Controllers;
 using Moq;
 using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
 
 namespace WeatherApi2Test
 {
@@ -13,10 +14,13 @@ namespace WeatherApi2Test
         [Fact]
         public  void Value_at_Get()
         {
+         
             var controller = new WeatherController();
-            var response =  controller.GetAll("-23.1234", "23.122");
-            response.Should().NotBeNull();
+            var response = controller.GetAll("58.3829152", "26.7320442");
+            Assert.Contains("Tartu", response.ToString());
+
         }
+        
 
         [Fact]
         public void True_FACT(){
